6578c7e378b53f86f19ad0e73e99cbb236a9c76d
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-02-02 23:07:21 +0900

    2023/02/02 end of learning session

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,4 @@
-{
+    /*
     let js = 'amazing';
 
     //these are values which are the smallest units in JS
@@ -12,6 +12,8 @@
     console.log(firstName);
     console.log(firstName);
 
+
+    //variable name conventions
     let jonas_matilda = "JM";
     let $function = 27;
 
@@ -28,4 +30,28 @@
     let job2 = "programmer"
 
     console.log(myFirstJob);
-}
+    */
+    // boolean is something that can only be true or false
+    let javascriptIsFun = true;
+    console.log(javascriptIsFun);
+
+
+    // console.log(typeof true);
+    console.log(typeof javascriptIsFun);
+    // console.log(typeof 23);
+    // console.log(typeof 'jonas');
+
+    //changing a boolean to a string by giving it a property
+    javascriptIsFun = "YES";
+    console.log(typeof javascriptIsFun);
+
+    //about undefined (when a variable is not assigned to anything)
+    let year;
+    console.log(year);
+    console.log(typeof year);
+
+    //
+    year = 1991;
+    console.log(typeof year);
+
+    console.log(typeof null);
