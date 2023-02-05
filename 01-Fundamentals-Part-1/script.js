2a94ecef742240efa7839a61bf3e598fe1798cc6
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-02-06 00:42:34 +0900

    2023/03/06 learning type conversio and coercion

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -238,3 +238,29 @@ GOOD LUCK 😀
 //     console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})!`);
 // }
 */
+
+// 2023/03/06
+//about type conversion and coercion
+
+//type conversion (Number / String)
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// //type coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('I am ' + '23' + ' years old');
+// console.log('I am ' + String(23) + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' * '2');
+
+// let n = '1' + 1;//starts with a string which changes it to a string = 11
+// n = n - 1;
+// console.log(n);
+// console.log(2 + 3 + 4 + '5');
+// console.log('10' - '4' - '3' - 2 + '5');
