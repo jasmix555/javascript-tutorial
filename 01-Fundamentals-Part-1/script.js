a605407be8db1f91302a38096709095332299eca
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-02-06 22:33:25 +0900

    2023/03/06 js tutorial update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -264,3 +264,49 @@ GOOD LUCK 😀
 // console.log(n);
 // console.log(2 + 3 + 4 + '5');
 // console.log('10' - '4' - '3' - 2 + '5');
+
+//about truthy and falsy values
+
+//falsy values : 0 , ' ' , undefined , null , NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+
+// const money = 0 ;
+// if(money){
+//     console.log("Don't spend it all");
+// }else{
+//     console.log("You should get a job");
+// }
+
+// let height;
+// if(height){
+//     console.log("YAY! Height is defined");
+// }else{
+//     console.log("Height is Undefined");
+// }
+
+//about equality operators : == vs ===
+//=== strict equal (use this more)
+//== loose equal (rarely use this)
+// const age = 18;
+// if(age === 18)console.log("You just became an adult");
+
+// const favourite = Number(prompt("What's your favourite number"));
+// console.log(favourite);
+
+// if( favourite === 23){
+//     console.log('Cool! 23 is an amazing number!');
+// }else if(favourite === 7){
+//     console.log("7 is also a cool number!");
+// }else{
+//     console.log("That is also a cool number");
+// }
+
+// //operator for different
+// if(favourite !== 23){
+//     console.log("Why not 23?");
+// }
+
+//boolean logic
