c58006e3f114a2be7d336cafed57d16533bde6c1
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-02-10 21:15:10 +0900

    switch statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -369,3 +369,30 @@ if(dolphinsScore > koalasScore &&  dolphinsScore >= 100){
 */
 
 //switch statement
+const day = "monday";
+switch(day){
+    case"monday": //day === "monday"
+    console.log("Plan course structure");
+    console.log('Go to coding meetup');
+    break;
+    case"tuesday":
+    console.log("Prepare theory videos");
+    break;
+    case"wednesday":
+    console.log("Work on the js challenge");
+    break;
+    case"thursday":
+    console.log("write code examples");
+    break;
+    case"friday":
+    console.log("Record videos");
+    break;
+    case"saturday":
+    console.log("enjoy the weekend's");
+    break;
+    case"saturday":
+    console.log("enjoy the weekend's");
+    break;
+    default:
+        console.log("Not a valid day");
+}
