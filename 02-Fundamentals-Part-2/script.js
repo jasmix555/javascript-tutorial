23cd404ca9263cd8c48fc536441387a39ec41f59
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-02-12 23:07:35 +0900

    2023/02/12 learning function

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,6 +1,25 @@
 'use strict';
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if(passTest) hasDriversLicense = true;
-if(hasDriversLicense) console.log("I can drive");
+// if(passTest) hasDriversLicense = true;
+// if(hasDriversLicense) console.log("I can drive");
+
+//learning functions
+function logger(){
+    console.log("My name is Jason");
+}
+
+//running the function
+logger();
+logger();
+logger();
+
+function fruitProcsr(apples , oranges){
+    console.log(apples , oranges );
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+const applejuice = fruitProcsr(5, 0);
+console.log(applejuice);
